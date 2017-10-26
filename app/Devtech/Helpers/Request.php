39b1676cdbfaf288eddd7077619664fd5c6a28a9
Nikola Zivkovic <nikola.zivkovic@devtechgroup.com>
2017-10-26 16:35:13 +0200

    Added PHPdoc to Request class.

--- app/Devtech/Helpers/Request.php
+++ app/Devtech/Helpers/Request.php
@@ -2,12 +2,23 @@
 
 namespace Devtech\Helpers;
 
+/**
+ * Class Request
+ *
+ * Returns uri and http method from HTTP requests.
+ */
 class Request
 {
+    /**
+     * Parses the uri from HTTP requests
+     *
+     * @return array  Contains http request method and parsed uri.
+     */
     public static function prepare()
     {
         $httpMethod = $_SERVER['REQUEST_METHOD'];
         $uri = $_SERVER['REQUEST_URI'];
+        var_dump($uri);
 
         if (false !== $pos = strpos($uri, '?')) {
             $uri = substr($uri, 0, $pos);
