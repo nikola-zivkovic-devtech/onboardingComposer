95eda30133893b654996713896f320f58a16bffb
Nikola Zivkovic <nikola.zivkovic@devtechgroup.com>
2017-10-31 13:43:34 +0100

    Added new route, connected the routing with controllers.

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -8,8 +8,8 @@ use Devtech\Helpers\Request;
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r)
 {
     $r->addRoute('GET', '/[index]', 'Welcome');
-    $r->addRoute('GET', '/about', 'About');
     $r->addRoute('GET', '/hello/{name:[a-zA-Z]+}', 'Hello');
+    $r->addRoute('GET', '/store/{item:[a-zA-Z]+}', 'Store');
 });
 
 Request::prepare();
